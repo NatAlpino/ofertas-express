2651638f050ddd403659aeafd45da1a021d65abe
Nat Alpino <nathyalpino@hotmail.com>
2026-09-08 18:20:33 -0300

    feat: initialize project

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+
+  {
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+  },
+
+  globalIgnores([
+    '.next/**',
+    'node_modules/**',
+    'out/**',
+    'dist/**',
+    'build/**',
+    'coverage/**',
+    'next-env.d.ts',
+  ]),
+])
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main>
+      <h1>Ofertas Express</h1>
+    </main>
+  );
+}
+
+export default Home;
