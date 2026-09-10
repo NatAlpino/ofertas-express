4f7a2b826a9abef6d8388d5a569d0cf23c957cee
Nat Alpino <nathyalpino@hotmail.com>
2026-09-10 17:18:56 -0300

    chore: update app icon and test setup

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,6 +1,6 @@
+import { server } from '@/mocks/server'
 import '@testing-library/jest-dom/vitest'
 import { beforeAll, afterEach, afterAll } from 'vitest'
-import { server } from '@/mocks/server'
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 
