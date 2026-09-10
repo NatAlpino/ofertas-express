1d34f5a73a229649e7f1ade0ca9b7cc9ad8e8b76
Nat Alpino <nathyalpino@hotmail.com>
2026-09-09 23:38:13 -0300

    feat: add msw handlers with node and browser clients

---
+++ src/mocks/browser.ts
@@ -0,0 +1,9 @@
+"use client";
+
+import { setupWorker } from "msw/browser";
+import { handlers } from "@/mocks/handlers";
+
+export function startMockWorker() {
+  const worker = setupWorker(...handlers);
+  return worker.start({ onUnhandledRequest: "bypass" });
+}
